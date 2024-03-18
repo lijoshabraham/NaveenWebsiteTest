7f826c73b78dcbf71998b0297469ac07727e040a
lijoshabraham <lijoshabraham96@gmail.com>
2024-03-18 10:47:43 -0400

    latest changes

--- src/main/java/com/naveenautomation/TestBases/TestBase.java
+++ src/main/java/com/naveenautomation/TestBases/TestBase.java
@@ -19,68 +19,69 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestBase {
 
-	private static Browsers DEFAULT_BROWSER = Browsers.EDGE;
-	private static Environment DEFAULT_ENV = Environment.PROD;
-	public static Logger logger;
-	private WebDriverEvents events;
-	private EventFiringWebDriver eDriver;
-	public static WebDriver driver;
+    private static Environment DEFAULT_ENV = Environment.PROD;
+    public static Logger logger;
+    private WebDriverEvents events;
+    private EventFiringWebDriver eDriver;
+    public static WebDriver driver;
 
-	@BeforeClass
-	public void setUpLogger() {
-		logger = Logger.getLogger(TestBase1.class);
-		PropertyConfigurator.configure("log4j.properties");
-		BasicConfigurator.configure();
-		logger.setLevel(Level.ALL);
-	}
+    @BeforeClass
+    public void setUpLogger() {
+        logger = Logger.getLogger(TestBase.class);
+        PropertyConfigurator.configure("log4j.properties");
+        BasicConfigurator.configure();
+        logger.setLevel(Level.ALL);
+    }
 
-	public void initialization() {
-		setBrowserForTesting();
-		driverManagement();
-		logger.info("Loading Page in Browser");
-		driver.get(DEFAULT_ENV.getEnvUrl());
-	}
+    public void initialization() {
+        setBrowserForTesting();
+        driverManagement();
+        logger.info("Loading Page in Browser");
+        driver.get(DEFAULT_ENV.getEnvUrl());
+    }
 
-	private void driverManagement() {
-		driver.manage().window().maximize();
-		// driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-	}
+    private void driverManagement() {
+        driver.manage().window().maximize();
+    }
 
-	private void setBrowserForTesting() {
-		switch (DEFAULT_BROWSER) {
-		case CHROME:
-			WebDriverManager.chromedriver().setup();
-			logger.info("Launching Chrome Browser");
-			driver = new ChromeDriver();
-			break;
-		case FIREFOX:
-			WebDriverManager.firefoxdriver().setup();
-			logger.info("Launching Firefox Browser");
-			driver = new FirefoxDriver();
-			break;
-		case EDGE:
-			WebDriverManager.edgedriver().setup();
-			logger.info("Launching Edge Browser");
-			driver = new EdgeDriver();
-			break;
+    private void setBrowserForTesting() {
+        String browserName = System.getProperty("browser");
 
-		default:
-			throw new IllegalArgumentException();
-		}
-		//Intialising Event Firing Webdriver
-				eDriver=new EventFiringWebDriver(driver);
-				
-				//Intialising Webdriver Events
-				events=new WebDriverEvents();
-				
-				//Register the event
-				eDriver.register(events);
-				driver=eDriver;
+        if (browserName == null) {
+            throw new IllegalArgumentException("Browser parameter is not provided");
+        }
 
-	}
+        Browsers browser = Browsers.valueOf(browserName.toUpperCase());
 
-	public void tearDown() {
-		driver.close();
-	}
+        switch (browser) {
+            case CHROME:
+                WebDriverManager.chromedriver().setup();
+                logger.info("Launching Chrome Browser");
+                driver = new ChromeDriver();
+                break;
+            case FIREFOX:
+                WebDriverManager.firefoxdriver().setup();
+                logger.info("Launching Firefox Browser");
+                driver = new FirefoxDriver();
+                break;
+            case EDGE:
+                WebDriverManager.edgedriver().setup();
+                logger.info("Launching Edge Browser");
+                driver = new EdgeDriver();
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported browser: " + browserName);
+        }
 
+        eDriver = new EventFiringWebDriver(driver);
+        events = new WebDriverEvents();
+        eDriver.register(events);
+        driver = eDriver;
+    }
+
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
